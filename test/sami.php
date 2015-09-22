b6aa9e126106beb68bdfea33c89e0aa089e930f9
Marcel Voigt <mv@noch.so>
2015-09-22 23:49:08 +0200

    New theme without bootstrap

---
+++ test/sami.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @copyright Copyright (c) 2015 Marcel Voigt
+ */
+use Sami\Parser\Filter\FilterInterface;
+use Sami\Reflection\ClassReflection;
+use Sami\Reflection\MethodReflection;
+use Sami\Reflection\PropertyReflection;
+use Sami\Sami;
+use Symfony\Component\Finder\Finder;
+
+$finder = Finder::create()
+    ->files()
+    ->name('*.php')
+//    ->in(__DIR__.'/../vendor')
+    ->in(__DIR__.'/../vendor/symfony/finder')
+//    ->in(__DIR__.'/../../cmdli/vendor')
+//    ->in(__DIR__.'/../vendor/twig')
+;
+
+$config = array(
+    'theme' => 'nochso',
+    'title' => 'Symfony Finder documentation using nochso/sami theme',
+    'build_dir' => __DIR__.'/build',
+    'cache_dir' => __DIR__.'/cache',
+    'template_dirs' => array(__DIR__.'/..'),
+);
+
+$sami = new Sami($finder, $config);
+$sami['filter'] = function () {
+    return new ProtectedFilter();
+};
+$fileGetFunction = new Twig_SimpleFunction('file_get_contents', 'file_get_contents');
+$sami['twig']->addFunction($fileGetFunction);
+
+class ProtectedFilter implements FilterInterface
+{
+    public function acceptClass(ClassReflection $class)
+    {
+        return true;
+    }
+
+    public function acceptMethod(MethodReflection $method)
+    {
+        return $method->isPublic() || $method->isProtected();
+    }
+
+    public function acceptProperty(PropertyReflection $property)
+    {
+        return $property->isPublic() || $property->isProtected();
+    }
+}
+
+return $sami;
